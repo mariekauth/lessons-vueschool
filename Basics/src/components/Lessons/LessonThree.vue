6f01e790c31b68efbb4ee2c4aae09ac938d23fe4
Marie Kauth <marie.kauth@gmail.com>
2023-09-26 02:54:56 -0400

    Lesson 3 complete

--- Basics/src/components/Lessons/LessonThree.vue
+++ Basics/src/components/Lessons/LessonThree.vue
@@ -2,12 +2,30 @@
 import { ref } from 'vue'
 
 const titleClass = ref('title')
+const red = (() => {titleClass.value = 'title-red'})
+const blue = (() => {titleClass.value = 'title-blue'})
+const green = (() => {titleClass.value = 'title-green'})
+const reset = (() => {titleClass.value = 'title'})
 </script>
 <template>
-  <h1>Make me red</h1> <!-- add dynamic class binding here -->
+  <h1 :class="titleClass">Make me red</h1> <!-- add dynamic class binding here -->
+  <button @click="red">Red</button><button @click="blue">Blue</button><button @click="green">Green</button><button @click="reset">Reset</button>
 </template>
 <style scoped>
 .title {
+  color: inherit;
+}
+.title-red {
   color: red;
+}
+.title-blue {
+  color: blue;
+}
+.title-green {
+  color: green;
+}
+
+button {
+    margin-right: 5px;
 }
 </style>
