7f79328b0186c3add00c8e7a2c64116cb90cd1c2
Marie Kauth <marie.kauth@gmail.com>
2023-09-26 04:21:31 -0400

    Complete Lesson six

--- Basics/src/components/Lessons/LessonSix.vue
+++ Basics/src/components/Lessons/LessonSix.vue
@@ -4,13 +4,14 @@ import { ref } from 'vue'
 const awesome = ref(true)
 
 function toggle() {
-  // ...
+  awesome.value = !awesome.value
 }
 </script>
 <template>
   <button @click="toggle">toggle</button>
-  <h1>Vue is awesome!</h1>
-  <h1>Oh no 😢</h1>
+  <h1 v-if="awesome">Vue is awesome!</h1>
+  <h1 v-else>Oh no 😢</h1>
+  Uses @click, v-if, v-else
 </template>
 <style scoped>
 </style>
