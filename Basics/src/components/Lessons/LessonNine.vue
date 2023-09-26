4dc57d31136741856629371dd667cce19b5fad64
Marie Kauth <marie.kauth@gmail.com>
2023-09-26 05:11:46 -0400

    Lesson nine complete

--- Basics/src/components/Lessons/LessonNine.vue
+++ Basics/src/components/Lessons/LessonNine.vue
@@ -1,11 +1,17 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 
 const pElementRef = ref(null)
+
+onMounted(() => {
+  pElementRef.value.textContent = 'mounted'
+})
 </script>
 
 <template>
   <p ref="pElementRef">hello</p>
+  <p>When you have dynamic content that is being loaded from a backend, you may need the onMounted method.</p>
+  <p>See <a href="https://vuejs.org/guide/essentials/lifecycle.html#lifecycle-diagram" target="_blank">the Lifecycle guide</a> for more information</p>
 </template>
 <style scoped>
 </style>
