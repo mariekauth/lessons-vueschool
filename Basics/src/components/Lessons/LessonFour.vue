d804593128da598b206d614ba6419fb2fcbf9d4f
Marie Kauth <marie.kauth@gmail.com>
2023-09-26 03:38:16 -0400

    Complete lesson 4

--- Basics/src/components/Lessons/LessonFour.vue
+++ Basics/src/components/Lessons/LessonFour.vue
@@ -2,10 +2,25 @@
 import { ref } from 'vue'
 
 const count = ref(0)
+
+function increment() {
+  // update component state
+  count.value++
+}
+
+function reset() {
+  count.value = 0;
+}
 </script>
 <template>
   <!-- make this button work -->
-  <button>count is: {{ count }}</button>
+  <button @click="increment">count is: {{ count }}</button>
+  <button @click="reset" v-if="count > 0">Reset</button>
+  <br />
+  Notice how this count is separate from the counter in lesson 2 due to scope.
 </template>
 <style scoped>
+button {
+  margin-right: 5px;
+}
 </style>
